f12d1e18733e1006c3cca7ae34a90395df3122bc
George Arnos <gjarnos@gmail.com>
2014-02-19 22:51:21 -0500

    2 19

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117032747) do
+ActiveRecord::Schema.define(version: 20140207030734) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -47,6 +47,9 @@ ActiveRecord::Schema.define(version: 20140117032747) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity"
+    t.string   "color"
+    t.string   "size"
   end
 
   create_table "users", force: true do |t|
