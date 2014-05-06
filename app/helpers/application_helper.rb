ab546b07492bdc2bbcd3343080e09009ebd12f8f
George Arnos <gjarnos@gmail.com>
2014-05-06 18:07:38 -0400

    more conf page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
     end
     content_tag("div", attributes, &block)
   end
+  
+  def hidden_div_confirm(condition, attributes = {}, &block)
+    if !condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,11 @@ require 'pry'
 
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
-  # ...
+  
   attr_accessor :stripe_card_token
 
   validates :name, :address, :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
@@ -19,11 +20,14 @@ class Order < ActiveRecord::Base
   end
 
   def process_and_save
+binding.pry
     if valid?
+binding.pry
       charge = Stripe::Charge.create(description: email, amount: (total_price * 100).to_i, card: stripe_card_token, currency: 'usd')
       self.stripe_charge_token = charge.id
       save!
     end
+
   rescue Stripe::InvalidRequestError => e
     logger.error "Stripe error while creating customer: #{e.message}"
     errors.add :base, "There was a problem with your credit card."
