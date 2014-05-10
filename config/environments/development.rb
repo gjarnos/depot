840cf1b5128d13667ea4c475f139ba291303bb32
George Arnos <gjarnos@gmail.com>
2014-05-10 16:25:15 -0400

    fonts7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,6 @@ Depot::Application.configure do
     enable_starttls_auto: true
   }
 
-  #config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
 end
